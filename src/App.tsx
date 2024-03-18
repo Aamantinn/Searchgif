697a132f58458241149be7ce21b9c7d4baf53007
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-18 23:51:21 +0100

    Initial commit: Setup project with Tailwind CSS, React Router, and React Query

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Routes>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
