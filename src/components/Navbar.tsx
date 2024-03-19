4aef6179b6abf25cf5a4b06e2d8b31c24ddbcb09
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 16:33:35 +0100

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from "react-router-dom";
+
+export const Navbar = () => {
+  const location = useLocation(); // Current location
+
+  // Function to determine if the link is active
+  const isActive = (pathname: string) => location.pathname === pathname;
+
+  return (
+    <nav className="sticky top-0 z-10 flex flex-col w-full px-6 py-4 font-sans text-center bg-blue-500 shadow sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
+      <div className="flex w-full justify-between">
+        <div className="flex items-center">
+          <Link to={`/`}>
+            <img className="w-28 " src="src\assets\GIPHY Transparent 27px.png" alt="Logo" />
+          </Link>
+        </div>
+        <div className="flex items-center gap-5">
+          <Link to={`/`}>
+            <span className={`flex uppercase flex-row items-center gap-2 text-base font-bold text-white no-underline ${isActive('/') ? 'bg-blue-700 ' : ''} hover:bg-blue-600 p-2 rounded`}>
+              Search
+            </span>
+          </Link>
+          <Link to={`/favorite`}>
+            <span className={`flex uppercase flex-row items-center gap-2 text-base font-bold text-white no-underline ${isActive('/favorite') ? 'bg-blue-700' : ''} hover:bg-blue-600 p-2 rounded`}>
+              Favorites
+            </span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
