cf9c8a0705a4a6f79a8634ad787107692312e372
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 14:54:12 +0100

    Refactor App.tsx to include FavoriteGif component and FavoritesProvider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import {Homepage} from "./pages/Homepage";
-
-
+import { Homepage } from "./pages/Homepage";
+import { FavoriteGif } from "./components/FavoriteGif";
+import { FavoritesProvider } from './context/FavoritesContext'; // Adjust the import path as necessary
 
 function App() {
-  
-
   return (
-    <>
-      
+    
+    <FavoritesProvider>
       <Routes>
-      <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/favorite" element={<FavoriteGif />} />
       </Routes>
-    </>
-  )
+    </FavoritesProvider>
+  );
 }
 
-export default App
+export default App;
