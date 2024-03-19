1a10485a73c73b49ee1d147deb5b81b8a5536b61
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 16:36:14 +0100

    Refactor FavoriteGif component to display a message when there are no favorites

--- src/components/FavoriteGif.tsx
+++ src/components/FavoriteGif.tsx
@@ -5,12 +5,16 @@ import { GifList } from './GifList'; // Assuming GifList is used for displaying 
 
 export const FavoriteGif: React.FC = () => {
   const { favorites } = useFavorites(); // Access favorites from context
-  console.log(favorites);
+  
 
-  return (
-    <div>
-      <h2>My Favorites</h2>
-      <GifList gifs={favorites} /> 
-    </div>
+  return (<>
+    <div className='flex flex-col justify-center text-center pt-8 '>
+      <h2 className="text-xl font-semibold">My Favorites</h2>
+      {favorites.length > 0 ? (
+        <GifList gifs={favorites} />
+      ) : (
+        <p className="text-center text-red-600 font-semibold">You have no favorites yet.</p> // Display a message when there are no favorites
+      )}
+    </div></>
   );
 };
