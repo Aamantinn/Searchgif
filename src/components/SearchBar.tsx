0f3f28c003cbed666495a514761dc4a03fe7aefc
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 13:50:15 +0100

    Refactor SearchBar component to accept a prop for setting the query

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useSearchGifs } from "../services/GiphyService"; // Import your custom hook
-import { IGif } from "../types/types";
+
+interface Props {
+    setQuery: (query: string) => void;
+}
 
 // Yup schema for validation
 const schema = yup
@@ -16,59 +18,23 @@ interface FormInputs {
   query: string;
 }
 
-export const SearchBar: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormInputs>({
-    resolver: yupResolver(schema),
-  });
+export const SearchBar: React.FC<Props> = ({ setQuery }) => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({
+        resolver: yupResolver(schema),
+    });
 
-  // useState to manage the form input for the query
-  const [inputQuery, setInputQuery] = useState("");
+    const onSubmit = (data: FormInputs) => {
+        setQuery(data.query);
+    };
 
-  // Custom hook using useQuery to fetch GIFs
-  const { data, isLoading, error } = useSearchGifs(inputQuery);
-
-  const onSubmit = (formData: FormInputs) => {
-    // Update inputQuery which is used by useSearchGifs to trigger the search
-    setInputQuery(formData.query);
-  };
-
-  return (
-    <>
-      <form
-        className="flex flex-col md:flex-row w-full p-10  gap-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <input
-          className="w-full px-2 py-2 border-2 rounded-md"
-          {...register("query")}
-          placeholder="Search GIFs"
-        />
-        {errors.query && <p>{errors.query.message}</p>}
-        <button
-          className="px-8 py-3  font-semibold rounded-md text-white text-lg bg-blue-500 hover:bg-blue-700"
-          type="submit"
-        >
-          Search
-        </button>
-      </form>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error fetching GIFs: </p>}{" "}
-      {/* Make sure to display the error message */}
-      <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-2 p-10">
-        {data?.data.map((gif: IGif) => (
-          <div className="w-full h-48 overflow-hidden relative">
-            <img
-              className="min-w-full min-h-full object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-              src={gif.images.fixed_height.url}
-              alt={gif.title}
-            />
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <div className="flex flex-col">
+        <form className="flex flex-col md:flex-row w-full p-10  gap-4" onSubmit={handleSubmit(onSubmit)}>
+            <input className="w-full px-2 py-2 border-2 rounded-md" {...register("query")} placeholder="Search GIFs" />
+            
+            <button className="px-8 py-3  font-semibold rounded-md text-white text-lg bg-blue-500 hover:bg-blue-700" type="submit">Search</button>
+        </form>
+        {errors.query && <p className="text-center text-red-600 font-semibold">{errors.query.message}</p>}
+        </div>
+    );
 };
