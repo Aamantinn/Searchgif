7b2b0d7836f3ceab7f7dc84740f37e1bfe4fa92b
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 13:50:28 +0100

    Refactor Homepage component to include search functionality and display GIFs

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,19 @@
-import { SearchBar } from '../components/SearchBar';
+// HomePage.tsx
+import React, { useState } from "react";
+import { SearchBar } from "../components/SearchBar";
+import { GifList } from "../components/GifList";
+import { useSearchGifs } from "../services/GiphyService";
 
+export const Homepage: React.FC = () => {
+    const [query, setQuery] = useState("");
+    const { data, isLoading, error } = useSearchGifs(query);
 
-export const Homepage = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <SearchBar setQuery={setQuery} />
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Error fetching GIFs: {error.message}</p>}
+            {!isLoading && !error && data && <GifList gifs={data.data} />}
+        </div>
+    );
+};
