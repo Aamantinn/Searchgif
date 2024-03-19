fd8d7518a929c07e11b63307e326f0dd8c18afc6
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 16:32:36 +0100

    Refactor Homepage component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,19 +3,18 @@ import React, { useState } from "react";
 import { SearchBar } from "../components/SearchBar";
 import { GifList } from "../components/GifList";
 import { useSearchGifs } from "../services/GiphyService";
-import { Link } from "react-router-dom";
+
 
 export const Homepage: React.FC = () => {
-    const [query, setQuery] = useState("");
-    const { data, isLoading, error } = useSearchGifs(query);
+  const [query, setQuery] = useState("");
+  const { data, isLoading, error } = useSearchGifs(query);
 
-    return (
-        <div><Link to="/favorite">Favorite</Link>
-          
-            <SearchBar setQuery={setQuery} />
-            {isLoading && <p>Loading...</p>}
-            {error && <p>Error fetching GIFs: {error.message}</p>}
-            {!isLoading && !error && data && <GifList gifs={data.data} />}
-        </div>
-    );
+  return (
+    <div className="bg-neutral-50 h-screen">
+      <SearchBar setQuery={setQuery} />
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error fetching GIFs: {error.message}</p>}
+      {!isLoading && !error && data && <GifList gifs={data.data} />}
+    </div>
+  );
 };
