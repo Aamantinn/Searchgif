ca983c3bb0bcb74f8e52219851f4df4a9896ef5a
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-19 01:21:28 +0100

    Export fetchGifs function in GiphyService.ts

--- src/services/GiphyService.ts
+++ src/services/GiphyService.ts
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query';
 import { IGiphyResponse } from '../types/types';
 
-const fetchGifs = async (query: string): Promise<IGiphyResponse> => {
+export const fetchGifs = async (query: string): Promise<IGiphyResponse> => {
   const API_KEY = 'vf7nDm11F3X2Pe63jIGjWWPiFCFCZXM8';
   const BASE_URL = 'https://api.giphy.com/v1/gifs';
   const url = `${BASE_URL}/search?api_key=${API_KEY}&q=${encodeURIComponent(query)}&limit=25`;
