5fd508aba37a76147574acee9d9d1ac30d4b5c6d
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-21 16:24:42 +0100

    Remove unnecessary code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/favorite" element={<FavoriteGif />} />
         </Routes>
-        
       </div>
       <Footer />
     </FavoritesProvider>
