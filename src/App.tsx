96116823359e36fea777406ddc0a4a12af0ab596
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-21 16:11:27 +0100

    Refactor import paths and add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
-import { FavoriteGif } from "./components/FavoriteGif";
-import { FavoritesProvider } from './context/FavoritesContext'; // Adjust the import path as necessary
+import { FavoriteGif } from "./pages/FavoriteGif";
+import { FavoritesProvider } from "./context/FavoritesContext"; // Adjust the import path as necessary
 import { Navbar } from "./components/Navbar";
-
-
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    
     <FavoritesProvider>
-     
       <Navbar />
-
-      <Routes>
+      <div className="min-h-screen bg-slate-50 flex flex-col justify-between">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/favorite" element={<FavoriteGif />} />
+        </Routes>
         
-        <Route path="/" element={<Homepage />} />
-        <Route path="/favorite" element={<FavoriteGif />} />
-      </Routes>
+      </div>
+      <Footer />
     </FavoritesProvider>
   );
 }
