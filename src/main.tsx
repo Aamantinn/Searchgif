9253fb6999b8059ef158ac81b2e93b1fb18ec3e3
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-21 17:49:11 +0100

    Update router to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
