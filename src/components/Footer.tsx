e3fff5a65c5a93194bd43ad1fad2da659ede9405
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-21 16:10:59 +0100

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+// Footer.tsx
+export const Footer: React.FC = () => {
+    return (
+      <footer className="bg-blue-500 text-center text-white  p-4 absolute  w-full">
+        © {new Date().getFullYear()} Amantin Qamili
+      </footer>
+    );
+  };
+  
+ 
