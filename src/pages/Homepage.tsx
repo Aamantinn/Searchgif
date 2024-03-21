978497b912aaa2f879ab8ae4296ab3cefaf811df
Amantin <amantin.qamili@edu.fh-kaernent.ac.at>
2024-03-21 16:10:02 +0100

    Refactor homepage layout

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -10,11 +10,12 @@ export const Homepage: React.FC = () => {
   const { data, isLoading, error } = useSearchGifs(query);
 
   return (
-    <div className="bg-neutral-50 w-full flex flex-col  mx-auto h-screen">
+    <div className=" w-full flex flex-col mx-auto h-full">
       <SearchBar setQuery={setQuery} />
       {isLoading && <div className="flex justify-center"> <p>Loading...</p></div>}
       {error && <div className="flex justify-center"><p>Error fetching GIFs: {error.message}</p></div>}
       {!isLoading && !error && data && <GifList gifs={data.data} />}
     </div>
+    
   );
 };
